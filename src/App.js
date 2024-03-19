39951fa2d27e075b4a609285fbc46f02b5f07e9f
Luka Martin <ldm3334@gmail.com>
2024-03-20 09:27:38 +1100

    Add useEffect to App component

--- src/App.js
+++ src/App.js
@@ -86,6 +86,12 @@ export default function App() {
           setGasNowValues(jsonResponse.data)
         })
   };
+  
+
+  useEffect(() => {
+    fetchGasValues()
+      fetchGasNowValues()
+  }, [])
 
 
   useEffect(() => {
