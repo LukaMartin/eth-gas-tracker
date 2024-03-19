9775a9cc2edc4c3a10d7c74f5f7fcdac7c7ccb31
Luka Martin <ldm3334@gmail.com>
2024-03-20 10:16:48 +1100

    revert previous change to GasNow layout

--- src/components/GasNowValues.js
+++ src/components/GasNowValues.js
@@ -17,7 +17,7 @@ export default function GasNow({ gasNowValues }) {
 
             <div className="border-4 border-red-500 rounded-2xl px-[2.1rem] ml-6 xl:ml-0">
                 <h3 className="text-xl pt-8 pb-8">REGULAR</h3>
-                <p className="text-md xl:text-lg pb-8 max-w-[4rem] pl-2"><span className="font-semibold">{(gasNowValues.standard / 1000000000).toFixed(2)}</span> GWei</p>
+                <p className="text-md xl:text-lg pb-8 max-w-[4rem] pl-6"><span className="font-semibold">{(gasNowValues.standard / 1000000000).toFixed(2)}</span> GWei</p>
             </div>
         </>
        
